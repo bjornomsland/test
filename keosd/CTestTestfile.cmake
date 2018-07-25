a0147cfb1672ffabcabb8b8c1927610d8410752f
Bjørn Omsland <39827237+bjornomsland@users.noreply.github.com>
2018-07-25 12:09:15 +0200

    Add files via upload

---
+++ keosd/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/c/eos-v1.0/eos/programs/keosd
+# Build directory: /mnt/c/eos-v1.0/eos/build/programs/keosd
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
